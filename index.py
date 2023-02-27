5decd86d9f15f55827651b713b7412087123c2a2
perazaf1 <pe.razafindrakoto@gmail.com>
2023-02-27 17:51:44 +0100

    third commit

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
 from plyer import notification
-from psutil import *
+import psutil
+import time
 
-notify (
-    
-)
+while(True):
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+
+
+    notification.notify (
+        title = "Pourcentage de la batterie",
+        message = str(percent) + "% de batterie restant",
+        app_icon = "Power.ico",
+        timeout = 20,
+        app_name="Pourcentage"
+    )
+    #répéter l'action toutes les heures
+    time.sleep(10*60)
+    continue
